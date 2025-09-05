c637eba8f499cea96d55be0554d0c834817fe587
kaoutarel <kaoutar.elazzab@gmail.com>
2025-09-04 22:56:32 -0400

    disable ESlint (recommendations block vercel)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // This will ignore ESLint errors during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
